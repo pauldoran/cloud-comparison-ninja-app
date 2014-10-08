79a98b6b9c1330ac301a59eb650cb1b0e5f745ef
Paul Doran <paul.doran@moneysupermarket.com>
2014-10-08 17:08:41 +0100

    Removing things references

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,29 +1,9 @@
 'use strict';
 
 angular.module('cloudComparisonNinjaApp')
-  .controller('MainCtrl', function ($scope, $http, $timeout, socket) {
+  .controller('MainCtrl', function ($scope, $timeout) {
     $scope.awesomeThings = [];
     $scope.ninjaLogo = false;
     $timeout(function(){$scope.ninjaLogo = true;}, 1000);
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-      socket.syncUpdates('thing', $scope.awesomeThings);
-    });
-
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
-        return;
-      }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
-    };
-
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
-    };
-
-    $scope.$on('$destroy', function () {
-      socket.unsyncUpdates('thing');
-    });
   });
