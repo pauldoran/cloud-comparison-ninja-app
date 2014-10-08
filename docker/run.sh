c0b015e868a889b35105b90b743619c89d643905
msmsimondean <simon.dean@moneysupermarket.com>
2014-10-08 16:29:50 +0100

    Update run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -3,7 +3,7 @@
 curl -sSL https://get.docker.io/ | bash
 
 crontab -l > mycron
-echo "echo */10 * * * * docker run -e $1 cloudcomparison.ninja:5000/perftest" >> mycron
+echo "*/10 * * * * docker run -e $1 cloudcomparison.ninja:5000/perftest" >> mycron
 crontab mycron
 rm mycron
 docker run -e $1 cloudcomparison.ninja:5000/perftest &
